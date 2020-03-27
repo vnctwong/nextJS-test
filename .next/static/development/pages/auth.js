87010120cb33f2729b16688121fa3c7d79385cd2
VWMAC <vincentkywong@gmail.com>
2020-03-26 22:36:37 -0700

    using nextJS for routing

---
+++ .next/static/development/pages/auth.js
@@ -0,0 +1,100 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/auth.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=%2FUsers%2Fvwmac%2FProjects%2FRCG%2F23.387%2FnextJS-test%2Fpages%2Fauth%2Findex.js!./":
+/*!***************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=%2FUsers%2Fvwmac%2FProjects%2FRCG%2F23.387%2FnextJS-test%2Fpages%2Fauth%2Findex.js ***!
+  \***************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/auth", function() {
+      var mod = __webpack_require__(/*! ./pages/auth/index.js */ "./pages/auth/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/auth/index.js */ "./pages/auth/index.js", function() {
+          if(!next.router.components["/auth"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/auth/index.js */ "./pages/auth/index.js")
+          next.router.update("/auth", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_2adc2403d89adc16ead0 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_2adc2403d89adc16ead0 */ "dll-reference dll_2adc2403d89adc16ead0"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/auth/index.js":
+/*!*****************************!*\
+  !*** ./pages/auth/index.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/vwmac/Projects/RCG/23.387/nextJS-test/pages/auth/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var authIndexPage = function authIndexPage() {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 3
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 5
+    }
+  }, "The Auth Index Page"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (authIndexPage);
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fauth&absolutePagePath=%2FUsers%2Fvwmac%2FProjects%2FRCG%2F23.387%2FnextJS-test%2Fpages%2Fauth%2Findex.js ***!
+  \*******************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fauth&absolutePagePath=%2FUsers%2Fvwmac%2FProjects%2FRCG%2F23.387%2FnextJS-test%2Fpages%2Fauth%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fauth&absolutePagePath=%2FUsers%2Fvwmac%2FProjects%2FRCG%2F23.387%2FnextJS-test%2Fpages%2Fauth%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_2adc2403d89adc16ead0":
+/*!*******************************************!*\
+  !*** external "dll_2adc2403d89adc16ead0" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_2adc2403d89adc16ead0;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=auth.js.map
